32225d0821c3944fec497e048ca02c3f5a3e71db
VighneshPath <pathrikarvighnesh@gmail.com>
2025-11-22 21:50:53 +0530

    Adds REACT agent with tool calls and loop

--- backend/app/agents/orchestrator/main.py
+++ backend/app/agents/orchestrator/main.py
@@ -1,53 +1,95 @@
-from typing import TypedDict, Dict, List, Union
+from typing import TypedDict, Annotated, Sequence
 from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import BaseMessage # Foundational class for all message types
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, ToolMessage
 from dotenv import load_dotenv
+from langgraph.graph.message import add_messages
+from langchain_core.tools import tool
 
 load_dotenv()
 
 class AgentState(TypedDict):
-    messages: List[Union[HumanMessage, AIMessage]]
+    messages: Annotated[Sequence[BaseMessage], add_messages]
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash-lite"
-)
+@tool
+def add(a: int, b: int):
+    """This is an addition function that adds two numbers together"""
+    return a+b
 
-def process(state: AgentState) -> AgentState:
-    """
-        This node will solve the request you input
-    """
+@tool
+def subtract(a: int, b: int):
+    """This is an subtract function that subtract two numbers together"""
+    return a-b
 
-    response = llm.invoke(state["messages"])
+@tool
+def multiply(a: int, b: int):
+    """This is an multiply function that multiply two numbers together"""
+    return a*b
 
-    state['messages'].append(AIMessage(content = response.content))
+tools = {add.name: add, subtract.name: subtract, multiply.name: multiply}
 
-    print(f"AI: {response.content}")
+model = ChatGoogleGenerativeAI(model = "gemini-2.0-flash-lite").bind_tools(list(tools.values()))
 
-    return state
 
+def model_call(state: AgentState) -> AgentState:
+    system_prompt = SystemMessage(content = "You are my AI assistant, please answer my query to the best of your ability")
+    response = model.invoke([system_prompt] + list(state["messages"]))
+
+    return {"messages": [response]}
+
+
+def should_continue(state: AgentState) -> str:
+    messages: Sequence[BaseMessage] = state["messages"]
+    last_message: BaseMessage = messages[-1]
+
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+def tool_node(state: AgentState) -> AgentState:
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool_fn = tools[tool_call["name"]]
+        observation = tool_fn.invoke(tool_call["args"])
+        result.append(ToolMessage(content=str(observation), tool_call_id=tool_call["id"]))
+
+    return {"messages": result}
 
 
 graph = StateGraph(AgentState)
 
-graph.add_node("process", process)
+graph.add_node("our_agent", model_call)
 
-graph.add_edge(START, "process")
-graph.add_edge("process", END)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END
+    }
+)
+
+graph.add_edge("tools", "our_agent")
 
 agent = graph.compile()
 
-conversation_history = []
+def print_stream(stream):
+    for s in stream:
+        message = s['messages'][-1]
+        if(isinstance(message, tuple)):
+            print(message)
+        else:
+            message.pretty_print()
 
-user_input = input("Enter: ")
 
+inputs = {"messages": [HumanMessage(content= "Add 34 + 21, Add 3+4, add 12 + 12, then subtract 9 from 1, and multiply 9 with 7. Then tell me a joke.")]}
 
-while user_input != 'exit':
-    conversation_history.append(HumanMessage(user_input))
-
-    result = agent.invoke({'messages': conversation_history})
-
-    print(result['messages'])
-    conversation_history = result['messages']
-
-    user_input = input("Enter: ")
+print_stream(agent.stream(inputs, stream_mode="values"))
